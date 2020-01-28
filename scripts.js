f9e375617090c9235561010e2f3e05f1a1649fbc
Adhal Ahmed <P17151661@my365.dmu.ac.uk>
2020-01-28 14:47:52 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const myMessage = document.getElementById(("message");
+myMessage.addEventListner("click", greetings);
+function greetings() {
+  if (myMessage.innerHTML=== "Click me") {
+  (myMessage.innerHTML = "Thanks!";
+} else{
+  myMessage.innerHTML="Click me";
+}
+}
